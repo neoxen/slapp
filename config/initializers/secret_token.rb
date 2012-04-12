71ae786426ce379134df83bb5c341f01151c1ab9
Neo Chen <neoxen@qq.com>
2012-04-12 21:32:41 +0800

    project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slapp::Application.config.secret_token = 'aa2e3db8a5c87dbbff781adbc86ad4e4804d78e94392889d32de78271d783b72d69b54d6e0c1966889d57b366c1c920073aa4549322b03fd6e3362793d05b2ef'
