9b4abf0b2efdaa1a2ad7c7d1f3e054cf2bd64a92
Neo Chen <neoxen@qq.com>
2012-08-30 22:52:10 +0800

    Add and remove tags for post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120829140916) do
+ActiveRecord::Schema.define(:version => 20120830132643) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(:version => 20120829140916) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "tags", ["post_id"], :name => "index_tags_on_post_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
