7cf60d70b4d8304d4c2738d5e7c6395f49475ba0
Neo Chen <neoxen@qq.com>
2013-07-08 23:41:25 +0800

    finished dishes_today

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,10 @@
 class IndexController < ApplicationController
   
   def index
-        
+    date = Time.now
+    date_today = date.strftime("%Y-%m-%d")
+    @dishes_today = Dish.find_all_by_is_today_and_set_date(true, date_today)
+
+    @last_post = Post.last
   end
 end
