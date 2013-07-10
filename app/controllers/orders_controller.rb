b20a0012d08339410092a491b5cf67fc9459c1fd
Neo Chen <neoxen@qq.com>
2013-07-10 23:31:45 +0800

    modified order index page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,18 @@ class OrdersController < InheritedResources::Base
     @user = current_user
     @orders = @user.orders
 
+    date = Time.now
+    date_today = date.strftime("%Y-%m-%d")
+    @dishes_today = Dish.find_all_by_is_today_and_set_date(true, date_today)
+
+    if current_user.account.nil?
+      user_account = Account.new(:amount => 0)
+      current_user.account = user_account
+    end
+
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @posts }
+      format.json { render json: @orders }
     end
   end
 end
