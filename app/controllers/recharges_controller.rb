f2b068a20b98eec394d688ffaa5a753f02b023ca
Neo Chen <neoxen@qq.com>
2013-08-06 11:17:56 +0800

    change recharge list from 5 to default 25

--- app/controllers/recharges_controller.rb
+++ app/controllers/recharges_controller.rb
@@ -2,6 +2,6 @@ class RechargesController < InheritedResources::Base
   before_filter :authorize_admin!, :except => [:index, :show]
 
   def index
-    @recharges = Recharge.page(params[:page]).per(5)
+    @recharges = Recharge.page(params[:page])
   end
 end
