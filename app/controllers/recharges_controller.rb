c94b34cba3eb2a8b9a804e804a2f9c49213181c5
Neo Chen <neoxen@qq.com>
2013-08-07 16:59:49 +0800

    recharge list ordering and remove edit/delete oprations

--- app/controllers/recharges_controller.rb
+++ app/controllers/recharges_controller.rb
@@ -2,6 +2,6 @@ class RechargesController < InheritedResources::Base
   before_filter :authorize_admin!, :except => [:index, :show]
 
   def index
-    @recharges = Recharge.page(params[:page])
+    @recharges = Recharge.order('recharge_date DESC').page(params[:page])
   end
 end
