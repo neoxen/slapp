cab58ee544dcdeb4354abb5f1dafa9fa739872e9
Neo Chen <neoxen@qq.com>
2015-06-01 12:51:22 +0800

    new styles for login and registration pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,4 +27,36 @@ $(document).ready(function() {
 	    $('#myModal').modal('show'); 
 	});
   });
+
+
+    //hide or show password
+    $('.hide-password').on('click', function(){
+        var $this= $(this),
+            $password_field = $this.prev('input');
+
+        ( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
+        ( 'Hide' == $this.text() ) ? $this.text('Show') : $this.text('Hide');
+        //focus and move cursor to the end of input field
+        $password_field.putCursorAtEnd();
+    });
 });
+
+
+
+
+//credits http://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
+jQuery.fn.putCursorAtEnd = function() {
+    return this.each(function() {
+        // If this function exists...
+        if (this.setSelectionRange) {
+            // ... then use it (Doesn't work in IE)
+            // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+            var len = $(this).val().length * 2;
+            this.setSelectionRange(len, len);
+        } else {
+            // ... otherwise replace the contents with itself
+            // (Doesn't work in Google Chrome)
+            $(this).val($(this).val());
+        }
+    });
+};
